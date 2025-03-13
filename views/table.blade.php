6c431df3a5559a1adcf08d16c12a2236b1eae5c0
Sadewawahyu <sadewawahyuprasadja@gmail.com>
2025-03-13 12:11:45 +0700

    Add files via upload

---
+++ views/table.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.template')
+
+@section('content')
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Kelas</th>
+            </tr>
+        </thread>
+
+        <thead>
+            <tr>
+                <th>1</th>
+                <th>Bagus</th>
+                <th>1233</th>
+                <th>B</th>
+            </tr>
+        </thread>
+
+        <thead>
+            <tr>
+                <th>2</th>
+                <th>candi</th>
+                <th>4342</th>
+                <th>B</th>
+            </tr>
+        </thread>
+
+        <thead>
+            <tr>
+                <th>3</th>
+                <th>Udin</th>
+                <th>9732</th>
+                <th>A</th>
+            </tr>
+        </thread>
+
+    </table>
+@endsection
