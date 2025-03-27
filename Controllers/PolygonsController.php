87a28386be29dd1d0617c3a95c63c29a276ef5bf
Sadewawahyu <sadewawahyuprasadja@gmail.com>
2025-03-27 13:29:50 +0700

    Add files via upload

---
+++ Controllers/PolygonsController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PolygonsModel;
+
+class PolygonsController extends Controller
+{
+    public function __construct()
+    {
+        $this->polygon = new PolygonsModel();
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [
+            'title' => 'Map',
+        ];
+
+        return view('map', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validation request
+        $request->validate([
+            'name' => 'required|unique:polygons,name',
+            'description' => 'required',
+            'geom_polygon' => 'required',
+        ],
+        [
+            'name.required' => 'Name is required',
+            'name.unique' => 'Name already exists',
+            'description.required' => 'Description is required',
+            'geom_polygon.required' => 'Geometry polygons is required',
+        ]
+        );
+
+        $data = [
+            'geom' => $request->geom_polygon,
+            'name' => $request->name,
+            'description' => $request->description,
+        ];
+
+        // Create data
+        if (!$this->polygon->create($data)) {
+            return redirect()->route('map')->with('error', 'Polygons failed to added');
+        }
+
+        // Redirect to map
+        return redirect()->route('map')->with('success', 'Polygons has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
